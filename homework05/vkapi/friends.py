22d299272cceaffb93ffc8bd5b240c5635ac1623
VladislavEvdokimov <cool.anonymus@bk.ru>
2021-01-29 14:08:31 +1000

    ready

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -76,22 +76,57 @@ def get_mutual(
     :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
     :param progress: Callback для отображения прогресса.
     """
-    if target_uids is None:
-        params = {
-            "access_token": VK_CONFIG["access_token"],
-            "v": VK_CONFIG["version"],
-            "source_uid": source_uid if source_uid is not None else "",
-            "target_uid": target_uid,
-            "order": order,
-        }
-        response = session.get("friends.getMutual", params=params)
-        if response.ok:
-            doc = response.json()
-        else:
-            raise APIError("HTTPError")
-        if "error" in doc:
-            raise APIError(doc["error"]["error_msg"])
-        return doc["response"]
+        source_uid: tp.Optional[int] = None,
+    target_uid: tp.Optional[int] = None,
+    target_uids: tp.Optional[tp.List[int]] = None,
+    order: str = "",
+    count: tp.Optional[int] = None,
+    offset: int = 0,
+    progress=tqdm,
+) -> tp.Union[tp.List[int], tp.List[MutualFriends]]:
+    """
+    Получить список идентификаторов общих друзей между парой пользователей.
+    :param source_uid: Идентификатор пользователя, чьи друзья пересекаются с друзьями пользователя с идентификатором target_uid.
+    :param target_uid: Идентификатор пользователя, с которым необходимо искать общих друзей.
+    :param target_uids: Cписок идентификаторов пользователей, с которыми необходимо искать общих друзей.
+    :param order: Порядок, в котором нужно вернуть список общих друзей.
+    :param count: Количество общих друзей, которое нужно вернуть.
+    :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
+    :param progress: Callback для отображения прогресса.
+    """
+ 
+    if target_uids:
+        x = []
+        y = int(len(target_uids) / 100) if int(len(target_uids) / 100) else 1
+        for t in progress(range(y)):
+            offset = 100 * t
+            r = session.get(
+                f"friends.getMutual?access_token={config.VK_CONFIG['access_token']}"
+                f"&source_uid={source_uid if source_uid else ''}"
+                f"&target_uids={','.join(map(str, target_uids)) if target_uids else ''}&order={order if order else ''}"
+                f"&count={count if count else ''}&offset={offset if offset else 0}"
+                f"&v={config.VK_CONFIG['version']}"
+            ).json()
+            try:
+                data = r["response"]
+            except KeyError:
+                raise APIError(r["error"])
+            x += [MutualFriends(**f) for f in data]  # type: ignore
+            if t % 3 == 2:
+                time.sleep(1)
+        return x
+    else:
+        r = session.get(
+            f"friends.getMutual?access_token={config.VK_CONFIG['access_token']}"
+            f"&source_uid={source_uid if source_uid else ''}&target_uid={target_uid if target_uid else ''}"
+            f"&order={order if order else ''}"
+            f"&count={count if count else ''}&offset={offset if offset else 0}"
+            f"&v={config.VK_CONFIG['version']}"
+        ).json()
+        try:
+            return r["response"]
+        except KeyError:
+            raise APIError(r["error"])
 
     responses = []
     if progress is None:
