2c9f54deaf21e33f058b1b8857fafd2ec2063391
VladislavEvdokimov <cool.anonymus@bk.ru>
2021-01-31 19:41:13 +1000

    fixed isort

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -1,6 +1,7 @@
 import random
 from typing import Tuple, List, Set, Optional
 
+
 def read_sudoku(filename: str) -> List[List[str]]:
     """ Прочитать Судоку из указанного файла """
     digits = [c for c in open(filename).read() if c in "123456789."]
