d50532d59d6ac71ecb9b161e052be8c89d4cec2d
VladislavEvdokimov <cool.anonymus@bk.ru>
2021-06-20 13:26:53 +0300

    hw07.4.0

--- homework07/example.py
+++ homework07/example.py
@@ -7,6 +7,7 @@ from tabulate import tabulate
 c = psycopg2.connect("host=localhost port=5432 dbname=odscourse user=postgres password=secret")
 curs = c.cursor()
 
+
 def fetch_all(curs):
     colnames = [desc[0] for desc in curs.description]
     records = curs.fetchall()
@@ -18,7 +19,7 @@ records = curs.fetchall()
 print("First 5 lines:")
 print(tabulate(records))
 
-#1. Сколько мужчин и женщин (признак sex) представлено в этом наборе данных?
+# 1. Сколько мужчин и женщин (признак sex) представлено в этом наборе данных?
 curs.execute(
     """
     SELECT sex, COUNT(*)
@@ -29,17 +30,20 @@ curs.execute(
 print("Amount of men and women:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#2. Каков средний возраст (признак age) женщин?
+# 2. Каков средний возраст (признак age) женщин?
 
-curs.execute("""
+curs.execute(
+    """
     SELECT AVG(age) FROM adult_data WHERE sex = 'Female'
-""")
+"""
+)
 print("Average women age:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#3. Какова доля граждан Германии (признак native-country)?
+# 3. Какова доля граждан Германии (признак native-country)?
 
-curs.execute( """
+curs.execute( 
+    """
     SELECT native_country, ROUND((COUNT(*) / (SELECT COUNT(*) FROM adult_data)::numeric), 6)
         FROM adult_data WHERE native_country = 'Germany'
         GROUP BY native_country;
@@ -48,38 +52,46 @@ curs.execute( """
 print("Share of Germans:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#4-5. Каковы средние значения и среднеквадратичные отклонения возраста тех, кто получает более 50K в год (признак *salary) и тех, кто получает менее 50K в год?
+# 4-5. Каковы средние значения и среднеквадратичные отклонения возраста тех, кто получает более 50K в год (признак *salary) и тех, кто получает менее 50K в год?
 
-curs.execute("""
+curs.execute(
+    """
     SELECT AVG(age), STDDEV(age) FROM adult_data WHERE salary = '>50K'
-""")
+"""
+)
 print("Average age and std deviation for >50K:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
 
-curs.execute("""
+curs.execute(
+    """
     SELECT AVG(age), STDDEV(age) FROM adult_data WHERE salary = '<=50K'
-""")
+"""
+)
 print("Average age and std deviation for <=50K:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#6. Правда ли, что люди, которые получают больше 50k, имеют как минимум высшее образование? (признак education – Bachelors, Prof-school, Assoc-acdm, Assoc-voc, Masters или Doctorate)
+# 6. Правда ли, что люди, которые получают больше 50k, имеют как минимум высшее образование? (признак education – Bachelors, Prof-school, Assoc-acdm, Assoc-voc, Masters или Doctorate)
 
-curs.execute("""
+curs.execute(
+    """
     SELECT education, salary FROM adult_data WHERE salary = '>50K'
     GROUP BY education, salary
-""")
+"""
+)
 print("Education of rich people:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#7. Выведите статистику возраста для каждой расы (признак race) и каждого пола. Используйте groupby и describe. Найдите таким образом максимальный возраст мужчин расы Amer-Indian-Eskimo.
+# 7. Выведите статистику возраста для каждой расы (признак race) и каждого пола. Используйте groupby и describe. Найдите таким образом максимальный возраст мужчин расы Amer-Indian-Eskimo.
 
-curs.execute("""
+curs.execute(
+    """
     SELECT COUNT(*),
            AVG(age), STDDEV(age), MIN(age), MAX(age), sex, race
     FROM adult_data
     GROUP BY race, sex
-""")
+"""
+)
 print("Age statistics:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
@@ -93,15 +105,17 @@ print(tabulate(fetch_all(curs), "keys", "psql"))
 
 #8. Среди кого больше доля зарабатывающих много (>50K): среди женатых или холостых мужчин (признак marital-status)? Женатыми считаем тех, у кого marital-status начинается с Married (Married-civ-spouse, Married-spouse-absent или Married-AF-spouse), остальных считаем холостыми.
 
-curs.execute("""
+curs.execute(
+    """
     SELECT ROUND((COUNT(*) / (SELECT COUNT(*) FROM adult_data)::numeric), 6), salary, marital_status
     FROM adult_data WHERE salary = '>50K'
     GROUP BY marital_status, salary
-            """)
+            """
+    )
 print("Impact of marital status:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-#9. Какое максимальное число часов человек работает в неделю (признак hours-per-week)? Сколько людей работают такое количество часов и каков среди них процент зарабатывающих много?
+# 9. Какое максимальное число часов человек работает в неделю (признак hours-per-week)? Сколько людей работают такое количество часов и каков среди них процент зарабатывающих много?
 
 curs.execute("SELECT MAX(hours_per_week::int) FROM adult_data")
 print("Max hours-per-week:")
@@ -116,33 +130,38 @@ curs.execute(
 print("Amount of people working max hours-per-week:")
 print(tabulate(fetch_all(curs), "keys", "psql"))
 
-curs.execute("""
+curs.execute(
+    """
     SELECT COUNT(*)
     FROM adult_data
     WHERE hours_per_week = '99' AND salary = '>50K'
     GROUP BY hours_per_week;
-""")
+"""
+)
 
-high_salary = fetch_all(curs)[0]['count']
+high_salary = fetch_all(curs)[0]["count"]
 
-curs.execute("""
+curs.execute(
+    """
     SELECT COUNT(*)
     FROM adult_data
     WHERE hours_per_week = '99'
     GROUP BY hours_per_week;
-""")
+"""
+)
 
-all = fetch_all(curs)[0]['count']
+all = fetch_all(curs)[0]["count"]
 print("Percentage of rich and hard-working:", round(high_salary / all * 100), '%')
 
-#10. Посчитайте среднее время работы (hours-per-week) зарабатывающих мало и много (salary) для каждой страны (native-country).
+# 10. Посчитайте среднее время работы (hours-per-week) зарабатывающих мало и много (salary) для каждой страны (native-country).
 
 curs.execute(
     """
     SELECT native_country, salary, ROUND(AVG(hours_per_week))
     FROM adult_data
     GROUP BY native_country, salary
-    """)
+    """
+    )
 
 # Среднее время работы для зарабатывающих мало и много в каждой стране
 print("Average hours-per-week for every country:")
--- homework07/pandas2.py
+++ homework07/pandas2.py
@@ -40,7 +40,9 @@ import numpy as np
 # Commented out IPython magic to ensure Python compatibility.
 import pandas as pd
 
-df = pd.read_csv("howpop_train.csv") #delimiter=",", names = ["url", "domain", "post_id", "published", "author", "flow", "polling", "content_len", "title", "comments", "favs", "views", "votes_plus", "votes_minus", "views_lognorm", "favs_lognorm", "comments_lognorm"])
+df = pd.read_csv(
+    "howpop_train.csv") 
+   #delimiter=",", names = ["url", "domain", "post_id", "published", "author", "flow", "polling", "content_len", "title", "comments", "favs", "views", "votes_plus", "votes_minus", "views_lognorm", "favs_lognorm", "comments_lognorm"])
 
 df.shape
 
@@ -61,9 +63,7 @@ df.describe(include=["object", "bool"]).T  # бинарные и категор�
 # настройка внешнего вида графиков в seaborn
 sns.set_style("dark")
 sns.set_palette("RdBu")
-sns.set_context(
-    "notebook", font_scale=1.5, rc={"figure.figsize": (15, 5), "axes.titlesize": 18}
-)
+sns.set_context("notebook", font_scale=1.5, rc={"figure.figsize": (15, 5), "axes.titlesize": 18})
 
 """Столбец **`published`** (время публикации) содержит строки. Чтобы мы могли работать с этими данными как с датой/временем публикации, приведём их к типу `datetime`:"""
 
@@ -102,18 +102,16 @@ april_2015
 april_2016 = df.query("year == 2016 & month == 4").shape[0]
 april_2016
 
-df.drop(filter(lambda c: c.endswith('_lognorm'), df.columns),
-        axis = 1,
-        inplace = True)
+df.drop(filter(lambda c: c.endswith('_lognorm'), df.columns),axis = 1,inplace = True)
 
-sns.set_style('white')
+sns.set_style("white")
 sns.set_palette("RdBu")
-sns.set_context("notebook", font_scale = 0.75,
-                rc = {"figure.figsize" : (30, 15), "axes.titlesize" : 12 })
+sns.set_context("notebook", font_scale = 0.75,rc = {"figure.figsize" : (30, 15), "axes.titlesize" : 12 })
 
-march_or_april = pd.DataFrame({"2015": [march_2015, april_2015], "2016": [march_2016, april_2016]}, index=["March", "April"])
-march_or_april.plot.bar(color=["deepskyblue", "dimgrey"],
-                       rot=0);
+march_or_april = pd.DataFrame(
+    {"2015": [march_2015, april_2015], "2016": [march_2016, april_2016]}, index=["March", "April"]
+    )
+march_or_april.plot.bar(color=["deepskyblue", "dimgrey"],rot=0);
 
 """## 2\. Проанализируйте публикации в месяце из предыдущего вопроса
 
@@ -126,11 +124,11 @@ march_or_april.plot.bar(color=["deepskyblue", "dimgrey"],
 Подсказки: постройте график зависимости числа публикаций от дня; используйте параметр `hue`; не заморачивайтесь сильно с ответами и не ищите скрытого смысла :)
 """
 
-df['dates'] = [str(p)[:7] for p in df.published]
+df["dates"] = [str(p)[:7] for p in df.published]
 list = df.dates.value_counts().index[0]
 df_popmonth = df[df.dates == list]
-df_popmonth['day'] = [p.day for p in df_popmonth.published]
-sns.countplot(x = 'day', data = df_popmonth)
+df_popmonth["day"] = [p.day for p in df_popmonth.published]
+sns.countplot(x = "day", data = df_popmonth)
 
 """## 3\. Когда лучше всего публиковать статью?
 
@@ -141,13 +139,13 @@ sns.countplot(x = 'day', data = df_popmonth)
 * На хабре дневные статьи комментируют чаще, чем вечерние
 """
 
-print(df.groupby('hour')['views'].mean().sort_values(ascending=False)[:5])
+print(df.groupby("hour")["views"].mean().sort_values(ascending=False)[:5])
 
-print(df.groupby('hour')['comments'].mean().sort_values(ascending=False)[:5])
+print(df.groupby("hour")["comments"].mean().sort_values(ascending=False)[:5])
 
-print(df[df.domain == 'habrahabr.ru'].groupby('hour')['comments'].mean().sort_values(ascending=False)[:5])
+print(df[df.domain == "habrahabr.ru"].groupby('hour')['comments'].mean().sort_values(ascending=False)[:5])
 
-df[df.domain == 'habrahabr.ru'].groupby('hour')[['comments']].mean().plot()
+df[df.domain == "habrahabr.ru"].groupby("hour")[["comments"]].mean().plot()
 
 """## 4\. Кого из топ-20 авторов чаще всего минусуют?
 
@@ -157,18 +155,23 @@ df[df.domain == 'habrahabr.ru'].groupby('hour')[['comments']].mean().plot()
 * @ilya42
 """
 
-df[df.author.isin(['@Mordatyj','@Mithgol', '@alizar', '@ilya42'])].groupby('author')[['votes_minus']].mean().sort_values('votes_minus', ascending = False)
+df[df.author.isin(["@Mordatyj","@Mithgol", "@alizar", 
+"@ilya42"])].groupby('author')[
+    ['votes_minus']
+].mean().sort_values('votes_minus', ascending = False)
 
-who = pd.DataFrame({"votes_minus": [20.481081, 7.928191, 7.471455, 6.216797]}, index=["@Mithgol", "@alizar", "@Mordatyj", "@ilya42"])
-who.plot.bar(color=["deepskyblue", "dimgrey", "mediumspringgreen", "orange"],
-                       rot=0);
+who = pd.DataFrame(
+    {"votes_minus": [20.481081, 7.928191, 7.471455, 6.216797]}, 
+    index=["@Mithgol", "@alizar", "@Mordatyj", "@ilya42"]
+    )
+who.plot.bar(color=["deepskyblue", "dimgrey", "mediumspringgreen", "orange"],rot=0);
 
 """## 5\. Сравните субботы и понедельники
 
 Правда ли, что по субботам авторы пишут в основном днём, а по понедельникам — в основном вечером?
 """
 
-fig = plt.figure(figsize=(20,20))
-fig.add_subplot(1,1,1)
-pl = sns.countplot(y = 'hour', hue = 'dayofweek', data = df[df.dayofweek.isin([1, 6])], palette="Set2")
-pl.set_title("Количество публикаций за час", fontweight = 'bold')
+fig = plt.figure(figsize=(20, 20))
+fig.add_subplot(1, 1, 1)
+pl = sns.countplot(y = "hour", hue = "dayofweek", data = df[df.dayofweek.isin([1, 6])], palette="Set2")
+pl.set_title("Количество публикаций за час", fontweight = "bold")
